aadbde9b2d5f04cf984a3a87e0f8a436d7d90ada
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-06 19:10:23 +1000

    organize colors for contributors

--- lua/tol/colors.lua
+++ lua/tol/colors.lua
@@ -1,6 +1,39 @@
 return {
+    -- syntax
+    blue = "#70b2f3",
+    comment = "#7e818b",
+    forest = "#bbcc33",
+    gray = "#dddddd",
+    green = "#44bb99",
+    orange = "#ee8866",
+    pink = "#ffaabb",
+    sky = "#99ddff",
+    taupe = "#cac0a9", -- default fg
+    yellow = "#eedd88",
+
+    -- brackets
+    cyan = "#01edf0",
+    lemon = "#f4dd64",
+    purple = "#ff4ee0",
+
+    -- ui
     added = "#96c065",
-    bg = "#292d3e",
+    cursor_pink = "#ff218c",
+    cursor_yellow = "#fbfc86",
+    deleted = "#f46a80",
+    diff_minus = "#542b30",
+    diff_plus = "#343d2a",
+    error = "#f46a80",
+    info = "#5ebdea",
+    modified = "#5ebdea",
+    search_bg = "#5e81ac",
+    search_fg = "#f8f6f1",
+    search_focused_bg = "#72aa00",
+    ui_gray = "#616E88", -- line numbers, inactive states
+    warning = "#f5c68f",
+
+    -- backgrounds and foregrounds
+    bg = "#292d3e", -- default bg
     bg_mono1 = "#33384d",
     bg_mono2 = "#3d435d",
     bg_mono3 = "#474e6c",
@@ -10,34 +43,9 @@ return {
     bg_tint1 = "#3e4251",
     bg_tint2 = "#535664",
     bg_tint3 = "#696c77",
-    blue = "#70b2f3",
-    comment = "#7e818b",
-    cursor_pink = "#ff218c",
-    cursor_yellow = "#fbfc86",
-    cyan = "#01edf0",
-    deleted = "#f46a80",
-    diff_minus = "#542b30",
-    diff_plus = "#343d2a",
-    error = "#f46a80",
-    fg_tint1 = "#e3d8be", -- these tints could be improved in consistency
+    fg_tint1 = "#e3d8be",
     fg_tint2 = "#fdf2d8",
     fg_tint3 = "#fdf3dc",
-    forest = "#bbcc33",
-    gray = "#dddddd",
-    green = "#44bb99",
-    info = "#5ebdea",
-    lemon = "#f4dd64",
-    modified = "#5ebdea",
+
     none = "NONE",
-    orange = "#ee8866",
-    pink = "#ffaabb",
-    purple = "#ff4ee0",
-    search_bg = "#5e81ac",
-    search_focused_bg = "#72aa00",
-    search_fg = "#f8f6f1",
-    sky = "#99ddff",
-    taupe = "#cac0a9",
-    ui_gray = "#616E88", -- line numbers, inactive states
-    warning = "#f5c68f",
-    yellow = "#eedd88",
 }
