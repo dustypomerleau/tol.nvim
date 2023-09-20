074d8c9076be7f4c3f9846c53f69bd3ed84f2f8b
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-20 13:28:11 +1000

    add flash scopes

--- lua/tol/theme.lua
+++ lua/tol/theme.lua
@@ -434,39 +434,14 @@ end
 theme.loadPlugins = function()
     return {
         -- Cmp
-        --
-        -- The following CompletionItemKind s can be customized with group:
-        -- CompletionItemKindMethod, etc.
-        --
-        -- Method = 2,
-        -- Function = 3,
-        -- Constructor = 4,
-        -- Field = 5,
-        -- Variable = 6,
-        -- Class = 7,
-        -- Interface = 8,
-        -- Module = 9,
-        -- Property = 10,
-        -- Unit = 11,
-        -- Value = 12,
-        -- Enum = 13,
-        -- Keyword = 14,
-        -- Snippet = 15,
-        -- Color = 16,
-        -- File = 17,
-        -- Reference = 18,
-        -- Folder = 19,
-        -- EnumMember = 20,
-        -- Constant = 21,
-        -- Struct = 22,
-        -- Event = 23,
-        -- Operator = 24,
-        -- TypeParameter = 25,
-
         CmpItemAbbrMatch = { fg = t.orange, bold = true, },
         CmpItemAbbrMatchFuzzy = { fg = t.orange, bold = true, },
-        -- CmpItemAbbr = { fg = t.taupe, },
-        -- CmpItemMenu = { fg = , },
+
+        -- Flash
+        FlashMatch = { link = "IncSearch", },
+        FlashCurrent = { link = "CurSearch", },
+        FlashBackdrop = { link = "Comment", },
+        FlashLabel = { fg = "yellow", bg = t.bg_mono3, },
 
         -- FZF Lua
         FzfLuaBorder = { fg = t.ui_gray, },
